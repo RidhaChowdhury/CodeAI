f4d5ed296a9998c57d4bcd26004f8d496d9efc90
Ridha Chowdhury <chowdhuryridha@gmail.com>
2024-01-16 16:23:07 -0500

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import { Textarea } from "@/components/ui/textarea"
+import './App.css'
+
+document.body.classList.add('dark');
+
+function App() {
+  return (
+    <>
+      <div>
+        <Textarea placeholder ="Type your message here."/>
+      </div>
+    </>
+  )
+}
+
+export default App
